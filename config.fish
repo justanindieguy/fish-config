eb8e60aae722742f4bdfdd79248bf85b96d109da
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2023-08-12 17:32:18 -0600

    add default custom config

---
+++ config.fish
@@ -0,0 +1,35 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+set -g fish_prompt_pwd_dir_length 1
+
+# aliases
+alias sudo "sudo "
+alias mv "mv -v"
+alias rm "rm -v"
+alias cp "cp -v"
+alias v "lvim"
+
+function echo_PATH
+  for dir in $PATH
+    echo $dir
+  end
+end
+
+if type -q exa
+  alias l "exa -lga --icons --group-directories-first"
+  alias ll "exa -lg --icons --group-directories-first"
+end
+
+set -gx EDITOR lvim
+set -gx BROWSER wslview
+set -gx GH_BROWSER wslview
+
+# NVM
+set -gx nvm_default_version 18.15.0
+set --query nvm_default_version &&
+nvm use $nvm_default_version >/dev/null
+
+# Oh my Posh!
+oh-my-posh init fish --config ~/.mintimb.omp.json | source
