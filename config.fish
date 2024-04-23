469229bf212af777dbdeaf4c09095d3a0e998d98
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-23 17:03:47 -0600

    add JAVA_HOME and M2_HOME environment variables

--- config.fish
+++ config.fish
@@ -4,6 +4,10 @@ set -gx TERM xterm-256color
 
 set -g fish_prompt_pwd_dir_length 1
 
+set -x JAVA_HOME /usr/lib/jvm/temurin-21-jdk-amd64
+
+set -x M2_HOME /opt/apache-maven-3.9.6
+
 # aliases
 alias sudo "sudo "
 alias mv "mv -v"
